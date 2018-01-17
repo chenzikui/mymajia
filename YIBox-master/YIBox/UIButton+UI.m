851e4e8222cc7ca0b686da70b7adf12ab26b32d0
chenzikui <czk19890516@qq.com>
2018-01-17 12:41:59 +0800

    add files

---
+++ YIBox-master/YIBox/UIButton+UI.m
@@ -0,0 +1,33 @@
+//
+//  UIButton+UI.m
+//  BaoBaoJi
+//
+//  Created by efeng on 15/12/26.
+//  Copyright © 2015年 buerguo. All rights reserved.
+//
+
+#import "UIButton+UI.h"
+
+@implementation UIButton (UI)
+
+- (void)orangeStyle {
+	[self setBackgroundColor:[UIColor cantaloupeColor]];
+	[self cornerStyle];
+	[self setTitleColor:kAppColorWhite forState:UIControlStateNormal];
+}
+
+- (void)greenStyle {
+	[self setBackgroundColor:[UIColor grassColor]];
+	[self cornerStyle];
+	[self setTitleColor:kAppColorWhite forState:UIControlStateNormal];
+}
+
+- (void)mainColorStyle {
+	[self setBackgroundColor:kAppColorMain];
+	[self cornerStyle];
+	[self setTitleColor:kAppColorWhite forState:UIControlStateNormal];
+}
+
+
+
+@end
